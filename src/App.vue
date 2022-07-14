bbaf0820fde31053d20528d61913cfe52660387b
Paul Nicholas <liquidream@hotmail.co.uk>
2022-07-14 08:00:48 +0100

    Added Vuetify 3 (beta) (Prev welcome page content/code commented)

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,32 @@
-<script setup lang="ts">
+<template>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+
+  components: {
+    HelloWorld,
+  },
+
+  data () {
+    return {
+      //
+    }
+  },
+})
+</script>
+
+
+<!-- <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import TheWelcome from "./components/TheWelcome.vue";
 </script>
@@ -50,4 +78,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
+
+
+/*
+BEFORE vuetify... 
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
@@ -9,3 +24,5 @@ const app = createApp(App);
 app.use(createPinia());
 
 app.mount("#app");
+
+*/
