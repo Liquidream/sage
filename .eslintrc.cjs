323aa2b7cadc084e753b745f9ac97e10b91b610d
Paul Nicholas <liquidream@hotmail.co.uk>
2022-07-14 20:37:31 +0100

    Re-added Pinia (+fixed eslint CR issue)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,4 +9,13 @@ module.exports = {
     "@vue/eslint-config-typescript/recommended",
     "@vue/eslint-config-prettier",
   ],
+
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        endOfLine: "auto",
+      },
+    ],
+  },
 };
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import { loadFonts } from './plugins/webfontloader'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import { loadFonts } from "./plugins/webfontloader";
 
-loadFonts()
+loadFonts();
 
-createApp(App)
-  .use(vuetify)
-  .mount('#app')
-
+createApp(App).use(vuetify).use(createPinia()).mount("#app");
 
 /*
 BEFORE vuetify... 
