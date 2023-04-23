786baec731fa60a9d12f9d22d512b930f2c8c6e3
Paul Nicholas <liquidream@hotmail.co.uk>
2023-04-23 11:17:46 +0100

    WIP exporting - trying to get static files in

--- src/pixi-sagedit/SAGExport.ts
+++ src/pixi-sagedit/SAGExport.ts
@@ -1,6 +1,5 @@
 import { saveAs } from "file-saver"
 import JSZip from "jszip"
-import { SAGEdit } from "./SAGEdit"
 import { useSceneStore } from "@/stores/SceneStore"
 import { Constants } from "@/constants"
 import type { SagePlayData } from "@/pixi-sageplay/SagePlayData"
@@ -10,6 +9,7 @@ import { usePropStore } from "@/stores/PropStore"
 import { useWorldStore } from "@/stores/WorldStore"
 import type { SceneState } from "@/stores/SceneStore"
 import type { ResolverManifest } from "pixi.js"
+import { playAssets } from "@/pixi-sageplay/playAssets"
 
 export class SAGExport {
   private constructor() {
@@ -22,13 +22,13 @@ export class SAGExport {
     console.log("SAGExport:initialize()...")
   }
 
-  public static performExport() {
+  public static async performExport() {
     console.log("SAGExport:performExport()...")
 
     // Create the zip file
     const zip = new JSZip()
 
-    const assetsManifest: ResolverManifest = {}
+    const assetsManifest = playAssets
 
     const playData = {} as SagePlayData
     playData.version = Constants.APP_VERSION
@@ -38,29 +38,33 @@ export class SAGExport {
     playData.worldData = JSON.stringify(useWorldStore().$state)
     // Scenes
     const sceneState = SAGExport.cloneState(useSceneStore()) as SceneState
-    debugger
-    playData.sceneData = SAGExport.exportSceneData(
-      sceneState,
-      assetsManifest,
-      zip
-    )
+    playData.sceneData = SAGExport.exportSceneData(sceneState, assetsManifest, zip)
     // playData.sceneData = JSON.stringify(useSceneStore().$state)
     playData.propData = JSON.stringify(usePropStore().$state)
     playData.doorData = JSON.stringify(useDoorStore().$state)
     playData.actorData = JSON.stringify(useActorStore().$state)
+
     const playDataJSON = JSON.stringify(playData, null, 2)
-
-    // Create the zip file
-    // const zip = new JSZip()
-    // zip.file("Hello.txt", "Hello World\n")
-    // const img = zip.folder("images")
-    // img?.file(`${sceneStore.scenes[0].id}.jpg`, imgData, { base64: true })
-
     zip.file("sageData.json", playDataJSON)
+
+    const assetsJSON = JSON.stringify(assetsManifest, null, 2)
+    zip.file("assets.json", assetsJSON)
+
+    // Core runtime files
+    fetch("/" + "index.html")
+      .then((res) => res.arrayBuffer())
+      .then((ab) => {
+        zip.file("index.html", ab)
+      })
+    fetch("/" + "entry-index.js")
+      .then((res) => res.arrayBuffer())
+      .then((ab) => {
+        zip.file("entry-index.js", ab)
+      })
 
     zip.generateAsync({ type: "blob" }).then(function (content) {
       // see FileSaver.js
-      saveAs(content, "example.zip")
+      saveAs(content, `${playData.id}-release.zip`)
     })
   }
 
@@ -72,52 +76,91 @@ export class SAGExport {
     console.log("Exporting scenes to zip...")
 
     const imgFolder = zip.folder("images")
+    const sfxFolder = zip.folder("sfx")
 
     for (const scene of sceneState.scenes) {
       // Export image to zip & replace state data with new filename
+
+      // Scene.Image
       const imgAssetName = `${scene.id}-image`
       const imgDataUri = scene.image || ""
-      SAGExport.exportImage(imgAssetName, imgDataUri, assets, imgFolder)
+      SAGExport.exportData(imgAssetName, imgDataUri, assets, imgFolder)
       scene.image = imgAssetName
 
-      const sfxFolder = zip.folder("sfx")
-
-      // Export image to zip & replace state data with new filename
+      // Scene.Sound
       const sfxAssetName = `${scene.id}-sound`
       const soundDataUri = scene.sound || ""
-      SAGExport.exportSound(sfxAssetName, soundDataUri, assets, sfxFolder)
+      SAGExport.exportData(sfxAssetName, soundDataUri, assets, sfxFolder)
       scene.sound = sfxAssetName
     }
 
     return JSON.stringify(sceneState)
   }
 
-  public static exportSound(
-    assetName: string,
-    soundDataUri: string,
-    assets: ResolverManifest,
-    zipImageFolder: JSZip | null
-  ): string {
-    const idx = soundDataUri.indexOf("base64,") + "base64,".length
-    const soundData = soundDataUri.substring(idx)
-    // TODO: Need to preserve original extensions
-    zipImageFolder?.file(`${assetName}.mp3`, soundData, { base64: true })
-    return assetName
-  }
+  // public static exportSound(
+  //   assetName: string,
+  //   soundDataUri: string,
+  //   assets: ResolverManifest,
+  //   zipImageFolder: JSZip | null
+  // ) {
+  //   const idx = soundDataUri.indexOf("base64,") + "base64,".length
+  //   const soundData = soundDataUri.substring(idx)
+  //   const fileExt = soundDataUri.split(';')[0].split('/')[1]
+  //   const filename = `${assetName}.${fileExt}`
+  //   // TODO: Need to preserve original extensions
+  //   zipImageFolder?.file(`${filename}`, soundData, { base64: true })
 
-  public static exportImage(
+  //   // Add to assets list
+  //   assets.bundles[0].assets.push({
+  //     name: assetName,
+  //     srcs: `${zipImageFolder?.root}${filename}`,
+  //   })
+  // }
+
+  // public static exportImage(
+  //   assetName: string,
+  //   imgDataUri: string,
+  //   assets: ResolverManifest,
+  //   zipImageFolder: JSZip | null
+  // ) {
+  //   // Find the offset to start of data
+  //   // https://github.com/Stuk/jszip/issues/404
+  //   const idx = imgDataUri.indexOf("base64,") + "base64,".length
+  //   const imgData = imgDataUri.substring(idx)
+  //   const fileExt = soundDataUri.split(';')[0].split('/')[1]
+  //   const filename = `${assetName}.${fileExt}`
+  //   // TODO: Need to preserve original extensions
+  //   zipImageFolder?.file(`${assetName}.jpg`, imgData, { base64: true })
+
+  //   // Add to assets list
+  //   assets.bundles[0].assets.push({
+  //     name: assetName,
+  //     srcs: `${zipImageFolder?.root}${assetName}.jpg`,
+  //   })
+
+  //   // TODO: Need to also return filetype (maybe do both here, then parse)
+  // }
+
+  public static exportData(
     assetName: string,
-    imgDataUri: string,
+    dataUri: string,
     assets: ResolverManifest,
-    zipImageFolder: JSZip | null
+    zipFolder: JSZip | null
   ) {
     // Find the offset to start of data
     // https://github.com/Stuk/jszip/issues/404
-    const idx = imgDataUri.indexOf("base64,") + "base64,".length
-    const imgData = imgDataUri.substring(idx)
+    const idx = dataUri.indexOf("base64,") + "base64,".length
+    const data = dataUri.substring(idx)
+    const fileExt = dataUri.split(';')[0].split('/')[1]
+    const filename = `${assetName}.${fileExt}`
     // TODO: Need to preserve original extensions
-    zipImageFolder?.file(`${assetName}.jpg`, imgData, { base64: true })
-    // TODO: Need to also return filetype (maybe do both here, then parse)
+    zipFolder?.file(filename, data, { base64: true })
+
+    // Add to assets list
+    assets.bundles[0].assets.push({
+      name: assetName,
+      srcs: `${zipFolder?.root}${filename}`,
+    })
   }
 
   public static cloneState(store: any): any {
