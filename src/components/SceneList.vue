c949c251e7b01d12b412853ff4552a668bbcaf8c
Paul Nicholas <liquidream@hotmail.co.uk>
2025-08-06 06:25:20 +0100

    Now with less/cleaner react code (SceneList)

--- src/components/SceneList.vue
+++ src/components/SceneList.vue
@@ -46,12 +46,14 @@
   import { storeToRefs } from "pinia"
 
   const worldStore = useWorldStore()
-  const worldRefs = storeToRefs(worldStore)
-  
+  //const worldRefs = storeToRefs(worldStore)
   
   const seq_scenes = computed(() => {
-    return worldRefs.getScenes.value.filter((scene) => scene.sequence_id === worldRefs.currSequenceId.value)
+    return worldStore.getScenes.filter((scene) => scene.sequence_id === worldStore.currSequenceId)
   })
+  // const seq_scenes = computed(() => {
+  //   return worldRefs.getScenes.value.filter((scene) => scene.sequence_id === worldRefs.currSequenceId.value)
+  // })
   //const seq_scenes = worldRefs.getScenes.value.filter((scene) => scene.sequence_id === worldRefs.currSequenceId.value)
   //const scenes = worldRefs.getScenes
 
